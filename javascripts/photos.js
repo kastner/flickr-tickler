2215c3ce0cb2059a5aad49266ab0e30b4b2261a8
Erik Kastner <kastner@gmail.com>
2008-12-06 12:42:00 -0500

    breaking out the javascript to it's own file

---
+++ javascripts/photos.js
@@ -0,0 +1,66 @@
+var currentPhotoNum = 1;
+var maxPhotoNum = 0;
+var pageNumber = 1;
+
+function getPhoto(num) {
+  return $("photo-" + num);
+}
+
+function currentPhoto() {
+  return getPhoto(currentPhotoNum);
+}
+
+function nextDivNum() {
+  for(i = 1; i <= maxPhotoNum; i++) {
+    if (getPhoto(i).viewportOffset().top > 0) { break; }
+  }
+  
+  return i;
+}
+
+function prevDivNum() {
+  nextNum = nextDivNum();
+  return (nextNum > 2) ? nextNum - 2 : 1;
+}
+
+function nextElement() {
+  currentPhotoNum = nextDivNum();
+  return currentPhoto();
+}
+
+function prevElement() {
+  currentPhotoNum = prevDivNum();
+  return currentPhoto();
+}
+
+Event.observe(window, 'keyup', function(event) {
+  switch(event.keyCode) {
+    case 74: // "J"
+      nextElement().scrollTo();
+      currentPhoto().down("a").focus();
+      if (maxPhotoNum - currentPhotoNum < 4) { fetchMorePhotos(); }
+      break;
+    case 75: // "K"
+      prevElement().scrollTo(); 
+      currentPhoto().down("a").focus();
+      break;
+  }
+});
+
+Ajax.Responders.register({
+  onCreate: function() { Ajax.activeRequestCount++; },
+  onComplete: function() { Ajax.activeRequestCount--; }
+});
+
+function fetchMorePhotos() {
+  if (Ajax.activeRequestCount > 0) { return; }
+  
+  new Ajax.Updater("photos", window.location + "?page=" + (pageNumber + 1), {
+    insertion: "bottom",
+    method: "get",
+    onComplete: function() {
+      pageNumber += 1;
+      maxPhotoNum += 15;
+    }
+  });
+}
